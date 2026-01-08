c70707e20b81133785cba1418ab0b1e2446ea26a
Alexey Selivanov <fateisdecease@gmail.com>
2026-01-08 20:15:23 +0300

    initial

---
+++ src/main/java/com/example/spring_boot_docker/SpringBootDockerApplication.java
@@ -0,0 +1,27 @@
+package com.example.spring_boot_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
+
+	@RequestMapping("/1")
+	public String home1() {
+		return "Hello World22";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
